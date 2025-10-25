c2267a93b813986345a6ff2f398a5e5bae2bdcf5
Narek Khurshudyan <narek@Nareks-MacBook-Pro.local>
2025-10-25 16:56:52 +0400

    feat: created initial files for Rush 02

---
+++ Piscine/Rushes/Rush-02/ex00/output.c
@@ -0,0 +1,25 @@
+#include "output.h"
+#include <unistd.h>
+
+void print_num_error()
+{
+    const char *error_msg = "Error\n";
+    write(2, error_msg, 6);
+}
+
+void print_dict_error()
+{
+    const char *error_msg = "Dict Error\n";
+    write(2, error_msg, 11);
+}
+
+void print_solution(const char *str)
+{
+    while (*str)
+    {
+        write(1, str, 1);
+        str++;
+    }
+    char new_line = '\n';
+    write(1, &new_line, 1);
+}
---
+++ Piscine/Rushes/Rush-02/ex00/output.h
@@ -0,0 +1,10 @@
+#ifndef OUTPUT_H
+#define OUTPUT_H
+
+#include <unistd.h>
+
+void print_num_error();
+void print_dict_error();
+void print_solution(const char *str);
+
+#endif
