0d11988691daf68e1caf4e630606cb22a400c650
Narek Khurshudyan <narek@Nareks-MacBook-Pro.local>
2025-10-26 23:30:26 +0400

    feat: romoved deleted files

--- Piscine/Rushes/Rush-02/ex00/input_check.h
+++
@@ -1,6 +0,0 @@
-#ifndef INPUT_CHECK_H
-#define INPUT_CHECK_H
-
-int is_valid_input(int args_count, const char **args, char **number_str, char **dict_path);
-
-#endif
--- Piscine/Rushes/Rush-02/ex00/number_builder.h
+++
@@ -1,6 +0,0 @@
-#ifndef NUMBER_BUILDER_H
-#define NUMBER_BUILDER_H
-
-int try_build_number_string(const char *number_str, const char *dict_path, const char **output_num);
-
-#endif
